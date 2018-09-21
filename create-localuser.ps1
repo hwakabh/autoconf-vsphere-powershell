cb6253ac5b638ecc7399eb9a4d73869ef1eafe70
hwakabh <hrykwkbys1024@gmail.com>
2018-09-21 16:41:58 +0900

    Translate comment messages from JP to ENG.

--- create-localuser.ps1
+++ create-localuser.ps1
@@ -1,19 +1,21 @@
-# hostlist.txt(ホストのIPリスト)が同一ディレクトリに存在している必要あり。
+# This Scripts is intended to create ESXi local user
+# Pre-Requirements :
+#   - hostlist.txt should be located in same directories
+#   - Write down the target ESXi to create user
+#   - Credentials of ESXi should be same.
 
 $ROOTUSER = "root"
 $ROOTPASS = "password"
 $ADDUSER = "ssmon"
 $ADDUSERPW = "password"
 
-
-#$esxisが配列としてESXiのIP情報を所持
+# Load list of ESXi from ./hostlist.txt
 $esxis = (Get-Content hostlist.txt) -as [string[]]
 
-#配列内から1つずつ取り出してループ
-#処理内容はユーザ作成およびAdmin権限の付与
 foreach ($esxi in $esxis) {
   Connect-VIServer $esxi -User $ROOTUSER -Password $ROOTPASS
   New-VMHostAccount -ID $ADDUSER -Password $ADDUSERPW  -UserAccount -GrantShellAccess
+  # Create users / Add Admin Permission
   New-VIPermission -Entity $esxi -Principal $ADDUSER -Role Admin -Propagate:$true
   Disconnect-VIServer $esxi -Confirm:$false
 }
