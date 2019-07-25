b51a691682ecae8c86dddfc35ef235d6850c355b
hwakabh <hrykwkbys1024@gmail.com>
2019-07-26 08:44:04 +0900

    Removed unused comments and added TODO.

--- create-localuser.ps1
+++ create-localuser.ps1
@@ -1,14 +1,11 @@
 # This Scripts is intended to create ESXi local user
-# Pre-Requirements :
-#   - hostlist.txt should be located in same directories
-#   - Write down the target ESXi to create user
-#   - Credentials of ESXi should be same.
 
 $ROOTUSER = "root"
 $ROOTPASS = "password"
 $ADDUSER = "ssmon"
 $ADDUSERPW = "password"
 
+# TODO: Get lists of hostname or FQDN with variable inputs.
 # Load list of ESXi from ./hostlist.txt
 $esxis = (Get-Content hostlist.txt) -as [string[]]
 
